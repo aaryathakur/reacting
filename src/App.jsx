3b470fe3fd5bfffc81844feeaae80fa287c8b21e
Aarya Thakur <aaryathakur1704@gmail.com>
2025-03-22 14:52:09 +0530

    Updated button syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
-import Card3 from './components/Card3'
+import React, { useState } from 'react'
+
 
 
 function App() {
-  return (
-    <>
-    <Card3 />
-    <h1>hey</h1>
-    </>
+  const [val, setVal] = useState(12);
+  return(
+    <div className='p-4'>
+      <h1 >{val}</h1>
+      <button onClick={() => setVal((prev) => prev + 1)} className='px-3 py-1 bg-blue-400 rounded-full'>
+  chnge karo
+</button>
+
+
+
+    </div>
   )
+    
 }
 
 export default App
