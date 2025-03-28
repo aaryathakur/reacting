a6b9c3d798ff0031515e921eba43e72e02be7ce1
Aarya Thakur <aaryathakur1704@gmail.com>
2025-03-29 03:05:59 +0530

    creating something

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import React ,{useState} from 'react';
+import React from "react";
+import Card from "./components/Card";  // Import the Card component
 
 function App() {
-  const [val, setVal] = useState([1,2,3,4,5,6])
-  return(
-    <div className='p-5'>
-  {val.map((item, index) => <h1 key={index}>{item}</h1> )}
-  <button onClick={() => setVal(() => val.filter((item => item%2 !== 0  )))} 
-    className='px-2 py-1 text-white rounded-full bg-blue-500'>
-    Click
-  </button>
-</div>
-
-  )
+  return (
+    <div>
+      <Card />   {/* Render the Card component */}
+    </div>
+  );
 }
 
 export default App;
