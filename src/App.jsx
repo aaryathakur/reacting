c3b6dc8d4393f5e6ef8877fd9355106e7d7f2e1b
Aarya Thakur <aaryathakur1704@gmail.com>
2025-03-28 23:39:40 +0530

    removing item

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import React, { useState } from 'react'
+import React ,{useState} from 'react';
 
 function App() {
-  const [val, setVal] = useState({name: " harsh" , age:24});
-  return (
-    <div>
-      <button onClick={async () => {
-        await setVal({...val, gender: "male" });
-        await console.log(val);
-      }} >click</button>
-    </div>
+  const [val, setVal] = useState([1,2,3,4,5,6])
+  return(
+    <div className='p-5'>
+  {val.map((item, index) => <h1 key={index}>{item}</h1> )}
+  <button onClick={() => setVal(() => val.filter((item => item%2 !== 0  )))} 
+    className='px-2 py-1 text-white rounded-full bg-blue-500'>
+    Click
+  </button>
+</div>
 
   )
-    
 }
 
-export default App
+export default App;
